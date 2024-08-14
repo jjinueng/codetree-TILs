b092846590916ceb06f27c7b44b7ac7481d920b8
jjinueng <115696442+jjinueng@users.noreply.github.com>
2024-08-14 15:01:31 +0900

    Title: 비교에 따른 연산, Time: 259ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240814/비교에 따른 연산/operation-based-on-comparison.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().split(" ").map(Number);
+
+if (a > b) {
+    console.log(a * b);
+} else {
+    console.log(Math.floor(b / a));
+}
